e0c2ec6cda67f64f3bd949eb7ad0c377e2d5957d
Otto von Sperling <ottok92@gmail.com>
2019-04-27 11:16:15 -0300

    MINOR CHANGES 	to how the AprenderScraper object is constructed. 	No initial username and password required.

--- scripts/scrape_for_answers.py
+++ scripts/scrape_for_answers.py
@@ -1,30 +1,54 @@
-import re, os
+import re, os, getpass
 from bs4 import BeautifulSoup
 import urllib.parse, urllib.request, urllib.error
 from http.cookiejar import CookieJar
 
-class AprenderScrapper:
+class AprenderScraper:
 	'''
 	'''
-	def __init__(self, username, password):
+	def __init__(self, course='', activity=''):
 		'''
-		Receives user data to for the scraping session
-		Returns reference to i)self-object created, ii)user's homepage BeautifulSoup object 
+		Returns reference to new AprenderScraper object
 		'''
-		if not username and not password:
-			os._exit(1)
-
-		self.username = username
-		self.password = password
+		self.username = ''
+		self.password = ''
 		self.cookiejar = CookieJar()
 		self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookiejar))
 		urllib.request.install_opener(self.opener)
 
+		# requests user homepage
+		self.userhome = self._authenticate()
 
-	def _authenticate(self):
+		# requests course homepage if argument provided in the constructor
+		if course:
+			self.coursehome = self.select_course(self.userhome, course)
+		else:
+			self.coursehome = ''
+
+		# requests activity page if argument provided in the constructor
+		if activity:
+			self.activitypage = self.select_activity(self.coursehome, activity)
+		else:
+			self.activitypage = ''
+
+
+	def _authenticate(self, username='', password=''):
 		'''
 		Logs in to the account and returns a BeautifulSoup object with the html data for the homescreen
 		'''
+		# requests username if none provided
+		if not username and not self.username:
+			self.username = input('Username (CPF): ')
+		else:
+			self.username = username
+
+		# requests password if none provided
+		if not password and not self.password:
+			self.password = getpass.getpass('Password: ')
+		else:
+			self.password = password
+
+		# authenticate and return BeautifulSoup object of the user's homepage
 		authentication_url = 'https://aprender.ead.unb.br/login/index.php'
 		payload = {'username': self.username,'password': self.password}
 		data = urllib.parse.urlencode(payload).encode("utf-8")
@@ -32,29 +56,38 @@ class AprenderScrapper:
 			return BeautifulSoup(resp.read().decode('utf-8'), 'html.parser')
 
 
-	def select_course(self, soup):
+	def select_course(self, soup, course=''):
 		'''
 		Receives an user's BeautifulSoup homepage object and return the equivalent object
 		for the course homepage
 		'''
-		# requests user input for course
-		course = input('Name or tag of course: ')
+		# requests user input for course if none provided
+		if not course:
+			course = input('Name or tag of course: ')
 
 		# gets a list of all courses listed on the user's homepage
+		courses = []
 		for div in soup.find_all('div', class_='coursebox'):
+			courses.append(div.h3.a.get_text)
 			if re.search(course, div.h3.a.get_text().lower()) != None:
 				url_coursehome = div.h3.a.get('href')
 				break
 
+		if not url_coursehome:
+			print('Course {} not found. Courses found: {}'.format(course, courses))
+			return  ''
+
 		return  BeautifulSoup(urllib.request.urlopen(url_coursehome), 'html.parser')
 
 
-	def select_activity(self, soup):
+	def select_activity(self, soup, activity=''):
 		'''
 		Receives a course's homepage soup and returns an activity's homepage
 		'''
-		# requests user input for activity
-		activity = input('Name or tag for activity: ')
+		# requests user input for activity if none provided
+		if not activity:
+			activity = input('Name or tag for activity: ')
+
 		for div in soup.find_all('div', class_='activityinstance'):
 			if re.search(activity, div.span.get_text().lower()) != None:
 				return BeautifulSoup(urllib.request.urlopen(div.a.get('href')), 'html.parser')
@@ -62,14 +95,9 @@ class AprenderScrapper:
 
 	def open_activity(self, soup):
 		'''
-		Receives a course's homepage, selects activity and opens it 
+		Receives an activity's homepage and opens it
+		Returns a BeautifulSoup object for the activity page 
 		'''
-		# requests the course homepage
-		soup = self.select_course(soup)
-
-		# requests the activity homepage
-		soup = self.select_activity(soup)
-
 		# submits payload to request activity (payload must be extracted from page)
 		payload = []
 		url_activity = soup.find('form').get('action')
@@ -78,3 +106,4 @@ class AprenderScrapper:
 		data = urllib.parse.urlencode(dict(payload)).encode("utf-8")
 		with urllib.request.urlopen(urllib.request.Request(url_activity, data)) as resp:
 			return BeautifulSoup(resp.read().decode('utf-8'), 'html.parser')
+
